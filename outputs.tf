84d9c96e7cb9f1f9f9ba135f5b0c1eba428787d4
Abdurahman Abdelgany <42511430+aaabdelgany@users.noreply.github.com>
2023-07-11 17:37:14 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "random_string_output_value" {
   value = "Application config output: ${random_string.id.result}"
-  description = "Our random output"
+  description = "Our random outputs"
 }
